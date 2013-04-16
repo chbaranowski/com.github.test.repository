da7e3a53f04eeba4a0105bcc4d18711c3d0a2d86
Christian Baranowski <Christian.Baranowski@seitenbau.com>
2013-04-16 15:03:43 +0200

    Change the code format to eclipse default

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,11 +1,11 @@
 public class Demo {
-	
+
 	public static void main() {
 		// TODO:
 	}
-	
+
 	public void soSomethingHere() {
-	    // TODO: The implementation
+		// TODO: The implementation
 	}
-	
+
 }
